9facc90216871149843712e5235fdd630e85f5df
nayan97 <nayanislam085@gmail.com>
2025-08-22 18:05:58 +0600

    conditional route done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,80 +1,102 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Navbar = () => {
+export default function NavBar() {
+  const { data: session, status } = useSession();
+  console.log(session);
+  const navMenu = () => {
+    return (
+      <>
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link href={"/services"}>Services</Link>
+        </li>{" "}
+        <li>
+          <Link href={"/blogs"}>Blogs</Link>
+        </li>{" "}
+        <li>
+          <Link href={"/my-bookings"}>My Bookings</Link>
+        </li>
+      </>
+    );
+  };
+
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+              {navMenu()}
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <Link href={"/"} className="text-xl">
+            <Image
+              src={"/assets/logo.jpg"}
+              width={107}
+              height={87}
+              alt="brand logo"
+            />
+          </Link>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navMenu()}</ul>
+        </div>
+        <div className="navbar-end">
+          <ul className="menu menu-horizontal px-1">
+            {status == "authenticated" ? (
+              <>
                 <li>
-                  <a>Submenu 1</a>
+                  <Image
+                    src={session?.user?.image}
+                    width={50}
+                    height={50}
+                    alt="user-logo"
+                  />
+                </li>
+                <li onClick={() => signOut()}>Log Out</li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link href={"/register"}>Register</Link>
                 </li>
                 <li>
-                  <a>Submenu 2</a>
+                  <Link href={"/login"}>Login</Link>
                 </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
+              </>
+            )}
+          </ul>
+          
+        </div>
       </div>
     </div>
   );
-};
-
-export default Navbar;
+}
